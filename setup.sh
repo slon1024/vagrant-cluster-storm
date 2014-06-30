8f9c2e4cf6e5eb1595395e812152d417cbaa0650
Vladimir <vova@getbase.com>
2014-06-30 21:33:25 +0200

    added function check_version

--- setup.sh
+++ setup.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 
-REQUIRED_VERSION_VAGRANT="1.5"
-VAGRANT_VERSION=$(vagrant --version | awk '{ print $2 }')
+check_version () {
+  first=$(echo $2 | tr -d '.')
+  second=$(echo $3 | tr -d '.')
+  if [ $(echo "$first < $second" | bc) -eq 1 ]
+  then
+    echo "required $1: ${3}+, actual is: $2"
+    exit
+  fi
+}
 
-if [ $(echo "${VAGRANT_VERSION%.*} >= ${REQUIRED_VERSION_VAGRANT}" | bc) -eq 0 ]
-then
-  echo "required vagrant: ${REQUIRED_VERSION_VAGRANT}+, actual is: ${VAGRANT_VERSION}"
-  exit
-fi
+
+VAGRANT_VERSION=$(vagrant --version | awk '{ print $2 }')
+REQUIRED_VAGRANT_VERSION="1.5.2"
+check_version "vagrant" $VAGRANT_VERSION $REQUIRED_VAGRANT_VERSION
+
 
 vagrant destroy -f
 vagrant up --no-provision
